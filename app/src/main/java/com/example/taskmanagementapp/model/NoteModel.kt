bfa70393fabcfee5ab0abc763d557ce60a197dbc
Udari Devindi <134499762+UdariDevindi@users.noreply.github.com>
2024-05-11 00:24:36 +0530

    created the user interface and the database

---
+++ app/src/main/java/com/example/taskmanagementapp/model/NoteModel.kt
@@ -0,0 +1,9 @@
+package com.example.taskmanagementapp.model
+
+data class NoteModel(
+
+    val id: Int,
+    val title: String,
+    val description: String
+
+)
---
+++ app/src/main/java/com/example/taskmanagementapp/utils/Constant.kt
@@ -0,0 +1,7 @@
+package com.example.taskmanagementapp.utils
+
+
+
+const val TABLE_NAME = "NoteTable"
+const val COLUMN_NAME_TITLE = "NoteTitle"
+const val COLUMN_NAME_DESCRIPTION = "NoteDescription"
